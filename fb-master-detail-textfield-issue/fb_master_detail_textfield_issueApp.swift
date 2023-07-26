bfe5236627ba6790e9579dc0beb7c9baf2d547ea
Andreas Seeger <asseeger@mac.com>
2023-07-26 22:35:06 +0200

    Initial Commit

---
+++ fb-master-detail-textfield-issue/fb_master_detail_textfield_issueApp.swift
@@ -0,0 +1,17 @@
+//
+//  fb_master_detail_textfield_issueApp.swift
+//  fb-master-detail-textfield-issue
+//
+//  Created by Andreas Seeger on 26.07.23.
+//
+
+import SwiftUI
+
+@main
+struct fb_master_detail_textfield_issueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
